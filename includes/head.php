9f1d50cea56cdd80193243642c5d9244c72b536c
Tom Swain <tom.s@designity.com>
2022-04-09 09:18:40 -0700

    Major Update (PHP Conversion)
    Website converted to PHP.

---
+++ includes/head.php
@@ -0,0 +1,31 @@
+<!doctype html>
+	<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Owl Carousel -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
+		<!-- Bootstrap CDN -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+		<!-- Custom Stylesheets -->
+		<link rel="stylesheet" href="css/structure.css">
+		<link rel="stylesheet" href="css/style.css">
+
+		<!-- Fonts -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700;900&family=Roboto:wght@100;200;300;400&display=swap" rel="stylesheet">
+
+		<!-- FontAwesome -->
+		<script src="https://kit.fontawesome.com/5ad74c9225.js" crossorigin="anonymous"></script>
+
+		<title>Das Keyboard - The Utimate Mechanical Keyboard Experience for Badasses</title>
+	</head>
+	<body>
---
+++ structure/header.php
@@ -0,0 +1,136 @@
+        <!-- Site Header -->
+        <section id="navSecondary" class="bg-gray-dark">
+            <div class="container-boxed">
+                <div class="row text-gray-medium d-flex align-items-center">
+                    <div class="col">
+                        <nav class="navbar p-0 navbar-expand-sm navbar-dark py-2 py-sm-0">
+                            <!-- Modal Trigger - Shipping Info -->
+                            <a class="shipping-notice w-75 pe-3 pe-md-0" href="#" title="Free Shipping" data-bs-toggle="modal" data-bs-target="#freeShipping">
+                                Free Shipping (Click for Details)
+                            </a>
+                            <!-- Modal - Shipping Info -->
+                            <div class="modal fade" id="freeShipping" tabindex="-1" aria-labelledby="freeShipping" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="freeShipping">Shipping Info</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <h2>Free Shipping Everyday on Orders Over $59</h2>
+                                            <p>All orders over $59 qualify for our Free Shipping offer within the continental United States. Keyboards are shipped via UPS Ground, and accessories and swag may be shipped via USPS. Once your cart meets this minimum amount, the Free Shipping option is automatically available during checkout. Other expedited shipping options will also be available at an additional charge.</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Learn More</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="navbar-toggler w-25 d-block d-sm-none collapsed" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="toggler-icon">
+                                    <span class="w-75 ms-auto"></span>
+                                    <span class="w-100"></span>
+                                    <span class="w-50 ms-auto"></span>
+                                </span>
+                            </a>
+                            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                                <ul class="navbar-nav ms-auto mt-3 mt-sm-0">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Support</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">About Us</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Wishlist</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="myAccount" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            My Account
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-end p-2" aria-labelledby="myAccount">
+                                            This is where the account menu will go
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <header id="navPrimary" class="bg-gray-darker sticky-top">
+            <div class="container-boxed">
+                <div class="row d-flex align-items-center">
+                    <div class="col">
+                        <nav class="row flex-column flex-sm-row navbar-expand navbar-dark">
+                            <a class="col-sm-5 navbar-brand mx-auto py-3 py-sm-4 pe-3 pe-sm-0 text-center text-sm-start" href="#">
+                                <img id="siteLogo" src="images/logo-das_keyboard-red_white.png" class="img-fluid" alt="Das Keyboard" />
+                            </a>
+                            <div class="col-sm-7 collapse navbar-collapse pb-3 py-sm-4" id="navPrimaryDropdown">
+                                <ul class="navbar-nav ms-sm-auto w-100 justify-content-around justify-content-sm-end">
+                                    <li id="navKeyboards" class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="keyboards" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Keyboards
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="keyboards">
+                                            <a class="dropdown-item" href="#">Smart RGB</a>
+                                            <a class="dropdown-item" href="#">For PC</a>
+                                            <a class="dropdown-item" href="#">For Mac</a>
+                                            <a class="dropdown-item" href="#">For Linux</a>
+                                            <a class="dropdown-item" href="#">Backlit</a>
+                                            <a class="dropdown-item" href="#">Refurbished</a>
+                                            <a class="dropdown-item" href="#">Best Sellers</a>
+                                            <hr>
+                                            <a class="dropdown-item bg-gr view-all py-3 d-flex align-items-center justify-content-between" href="#">View All<i class="fa-solid fa-plus text-red"></i></a>
+                                        </div>
+                                    </li>
+                                    <li id="navAccessories" class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="accessories" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Accessories
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="accessories">
+                                            <a class="dropdown-item" href="#">Switches</a>
+                                            <a class="dropdown-item" href="#">Keycaps</a>
+                                            <a class="dropdown-item" href="#">Parts</a>
+                                            <a class="dropdown-item" href="#">Swag</a>
+                                            <hr>
+                                            <a class="dropdown-item view-all py-3 d-flex align-items-center justify-content-between" href="#">View All<i class="fa-solid fa-plus text-red"></i></a>
+                                        </div>
+                                    </li>
+                                    <li id="navSoftware" class="nav-item">
+                                        <a class="nav-link" href="#">Software</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+                <hr class="m-0">
+            </div>
+        </header>
+        <section id="navBreadcrumbs" class="bg-gray-darker">
+            <div class="container-boxed">
+                <div class="row">
+                    <div class="col d-md-flex align-items-center justify-content-between">
+                        <nav class="breadcrumb m-0 bg-gray-darker px-0 d-flex justify-content-center justify-content-lg-start" aria-label="breadcrumb">
+                            <ol class="breadcrumb m-0 bg-gray-darker px-0">
+                                <li class="breadcrumb-item home pe-1" class="breadcrumb-item"><a href="#"><i class="fa-solid fa-house"></i></a></li>
+                                <li class="breadcrumb-item"><a href="#">Keyboards</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Das Keyboard 4 Professional</li>
+                            </ol>
+                        </nav>
+                        <div id="headerCTA" class="pb-3 py-md-4">
+                            <div class="col d-flex align-items-center justify-content-center justify-content-md-between pe-md-0">
+                                <a class="btn btn-blue me-sm-1" href="#" title="View All Keyboards"><span class="d-none d-sm-inline">Shop </span>Keyboards</a>
+                                <a class="btn btn-outline-primary ms-2" href="#" title="Download Software"><span class="d-none d-sm-inline">Download</span> Software<i class="fa-solid fa-download ms-2"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
