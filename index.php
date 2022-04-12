30d2e7b3f0839dd1776bf8f87a74d31b262d9e59
Tom Swain <tom.s@designity.com>
2022-04-12 14:39:19 -0700

    PHP Conversion

---
+++ index.php
@@ -0,0 +1,409 @@
+<?php include 'includes/head.html';?>
+	<?php include 'structure/header.html';?>
+		<!-- Hero Section -->
+		<section id="hero" class="bg-gray-darker text-white pb-sm-5 d-flex flex-column">
+			<div class="container-boxed">
+				<div class="row">
+					<div class="col-md-9 col-xl-6">
+						<figure class="heading mt-lg-0">
+							<figcaption class="text-white">
+								<h1>
+									<small>The Ultimate</small>
+									<br>Mechanical Keyboard
+								</h1>
+							</figcaption>
+							<p>Das Keyboard offers badass geeks the ultimate experience with high-performance mechanical keyboards with superior durability, construction, 	and design.</p>
+						</figure>
+					</div>
+				</div>
+			</div>
+			<div class="container-fluid home-hero-image p-0">
+				<div class="row g-0">
+					<img class="img-fluid" src="images/img-hero-home.png" />
+				</div>
+			</div>
+		</section>
+
+		<!-- Keyboard Categories -->
+		<section id="categoryKeyboards" class="bg-white pt-5 pb-3">
+			<div class="container-boxed">
+				<div class="row mt-5">
+					<div class="col">
+						<figure class="heading text-center mb-0">
+							<figcaption>
+								<h1>
+									<small>A Mechanical Keyboard</small>
+									<br>For Every Badass.
+								</h1>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+				<div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 gy-2">
+					<div class="col">
+						<figure class="heading card">
+							<div class="card-body">
+								<img src="images/cat-mechanical_keyboards-smart_rgb.png" class="card-img" alt="Smart RGB" />
+								<figcaption class="card-img-overlay">
+									<h1 class="card-title">
+										<small>Das Keyboard</small>
+										<br>Smart RGB
+									</h1>
+									<a class="btn btn-outline-primary" href="#" title="View Keyboards"><span>View Keyboards<i class="fa-solid fa-plus text-red ms-4"></span></i></a>
+								</figcaption>
+							</div>
+						</figure>
+					</div>
+					<div class="col">
+						<figure class="heading card">
+							<div class="card-body">
+								<img src="images/cat-mechanical_keyboards-for_pc.png" class="card-img" alt="Smart RGB" />
+								<figcaption class="card-img-overlay">
+									<h1 class="card-title">
+										<small>Das Keyboard</small>
+										<br>For PC
+									</h1>
+									<a class="btn btn-outline-primary" href="#" title="View Keyboards"><span>View Keyboards<i class="fa-solid fa-plus text-red ms-4"></span></i></a>
+								</figcaption>
+							</div>
+						</figure>
+					</div>
+					<div class="col">
+						<figure class="heading card">
+							<div class="card-body">
+								<img src="images/cat-mechanical_keyboards-for_mac.png" class="card-img" alt="Smart RGB" />
+								<figcaption class="card-img-overlay">
+									<h1 class="card-title">
+										<small>Das Keyboard</small>
+										<br>For Mac
+									</h1>
+									<a class="btn btn-outline-primary" href="#" title="View Keyboards"><span>View Keyboards<i class="fa-solid fa-plus text-red ms-4"></span></i></a>
+								</figcaption>
+							</div>
+						</figure>
+					</div>
+					<div class="col">
+						<figure class="heading card">
+							<div class="card-body">
+								<img src="images/cat-mechanical_keyboards-for_linux.png" class="card-img" alt="Smart RGB" />
+								<figcaption class="card-img-overlay">
+									<h1 class="card-title">
+										<small>Das Keyboard</small>
+										<br>For Linux
+									</h1>
+									<a class="btn btn-outline-primary" href="#" title="View Keyboards"><span>View Keyboards<i class="fa-solid fa-plus text-red ms-4"></span></i></a>
+								</figcaption>
+							</div>
+						</figure>
+					</div>
+					<div class="col">
+						<figure class="heading card">
+							<div class="card-body">
+								<img src="images/cat-mechanical_keyboards-backlit.png" class="card-img" alt="Smart RGB" />
+								<figcaption class="card-img-overlay">
+									<h1 class="card-title">
+										<small>Das Keyboard</small>
+										<br>Backlit
+									</h1>
+									<a class="btn btn-outline-primary" href="#" title="View Keyboards"><span>View Keyboards<i class="fa-solid fa-plus text-red ms-4"></span></i></a>
+								</figcaption>
+							</div>
+						</figure>
+					</div>
+					<div class="col">
+						<figure class="heading card">
+							<div class="card-body">
+								<img src="images/cat-mechanical_keyboards-refurbished.png" class="card-img" alt="Smart RGB" />
+								<figcaption class="card-img-overlay">
+									<h1 class="card-title">
+										<small>Das Keyboard</small>
+										<br>Refurbished
+									</h1>
+									<a class="btn btn-outline-primary" href="#" title="View Keyboards"><span>View Keyboards<i class="fa-solid fa-plus text-red ms-4"></span></i></a>
+								</figcaption>
+							</div>
+						</figure>
+					</div>
+				</div>
+			</div>			
+		</section>
+
+		<!-- Software Section -->
+		<section id="theSoftware" class="pt-5 bg-gray-lighter">
+			<div class="container-boxed">
+				<div class="row">
+					<div class="col-lg-8 offset-lg-2 col-xxl-10 offset-xxl-1">
+						<figure class="heading text-center">
+							<figcaption>
+								<h1>
+									<small>The Software</small>
+									<br>Das Keyboard Q
+								</h1>
+							</figcaption>
+							<p>Das Keyboard Q Software turns an RGB keyboard into a smart keyboard whose keys change colors based on information trends such as weather, stock quotes, project statuses (e.g. Trello, Basecamp, etc.). Customize your keyboard to meet your needs and stay informed on what is important to you.</p>
+							<a class="btn btn-outline-primary text-gray-dark ms-2" href="#" title="Download Software">Download Software<i class="fa-solid fa-download ms-2"></i></a>
+						</figure>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col video-col">
+
+						<!-- Mobile Only -->
+						<img class="video-mockup img-fluid d-md-none" src="images/img-home-software_mockup.png" alt="Software Mockup" />
+
+						<span class="video-shine d-none d-md-block"></span>
+						<video class="d-block mx-auto w-100 d-none d-md-block" autoplay loop muted poster="images/img-home-software_mockup.png">
+						<source src="video/q-applet.mp4" type="video/mp4"></video>
+						<img class="video-base d-none d-md-block mx-auto position-relative" src="images/img-home-video_base.png" alt="Video Base" />
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Values Section -->
+		<section id="values" class="bg-gray-darker dark-section pt-5">
+			<div class="container-boxed pt-md-5 mt-xl-5">
+				<div class="row mt-xxl-5">
+					<div class="col-md-6 order-md-2 d-md-flex align-items-end align-items-xxl-start">
+						<figure class="heading text-center mt-5 mt-md-3 pt-md-5">
+							<figcaption>
+								<h1>Das Keyboard <span class="text-red">Values</span></h1>
+							</figcaption>
+							<p>We are a badass company that makes badass products for badass customers. We are quality obsessed and develop all our products with premium materials.</p>
+							<a class="btn btn-primary" href="values.html" title="Read More">Read More<i class="fa-solid fa-caret-right ms-2"></i></a>
+						</figure>
+					</div>
+					<div class="col-md-6 order-md-1 d-md-flex align-items-center position-relative">
+						<img class="full-image position-absolute d-none d-xxl-inline" src="images/img-home-values-full.png" alt="Das Keybaord Values" />
+						<img class="half-image img-fluid" src="images/img-home-values.png" alt="Das Keybaord Values" />
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Switches Section -->
+		<section id="switches" class="pt-5 mt-5 bg-white">
+			<div class="container-boxed pb-5 pt-xl-5 mt-sm-5">
+				<div class="row mb-md-4">
+					<div class="col-lg-10 offset-lg-1 col-xl-8 offset-xl-2">
+						<figure class="heading text-center">
+							<figcaption>
+								<h1>Long lasting, <span class="text-red">durable switches</span>. </h1>
+							</figcaption>
+							<p>All of our keyboards are designed with high-performance, gold-plated mechanical key switches. Learn more about the different switches Das Keyboard offers.</p>
+							<a class="btn btn-outline-primary text-gray-dark ms-2" href="#" title="View Switches">View Switches<i class="fa-solid fa-caret-right ms-2"></i></a>
+						</figure>
+					</div>
+				</div>
+			</div>
+			<div class="container-fluid bg-gray-darker">
+				<div class="row">
+					<div class="col-sm-8 offset-sm-2">
+						<img class="d-block mx-auto mb-4 mb-md-5 mw-100" src="images/img-home-switches.png" alt="Long lasting, durable switches." />
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Font Section -->
+		<section id="font" class="py-5 bg-gray-lighter">
+			<div class="container-boxed">
+				<div class="row">
+					<div class="col-xl-10 offset-xl-1 d-md-flex align-items-center justify-content-between">
+						<img class="img-fluid d-block mx-auto mb-4 mb-md-0 pe-md-5" src="images/img_blueprint-D.png" alt="Blueprint Letter D" />
+						<figure class="heading text-center text-md-start ps-lg-5">
+							<figcaption>
+								<h1>A brand new <span class="text-red">keyboard font</span>.</h1>
+							</figcaption>
+							<p>Das Keyboard’s key cap font has been specially designed to provide ease of reading and harmonious view of the keyboard key caps. Das Keyboard’s font looks modern while keeping some of its lines from older, well established fonts. The overall result is sleek and easily readable while not drawing unnecessary attention.</p>
+						</figure>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Reviews Section -->
+		<section id="reviews" class="py-5 bg-white">
+			<div class="container-boxed">
+				<div class="row">
+					<div class="col">
+						<figure class="heading text-center">
+							<figcaption>
+								<h1>Real Reviews <span class="text-red d-block d-sm-inline">from real people</span></h1>
+							</figcaption>
+							<p>Our customers love Das Keyboard. But don't take our word for it...</p>
+						</figure>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<!-- Reviews Tabs -->
+						<ul class="nav nav-tabs d-flex align-items-center justify-content-between oswald mb-5">
+							<li class="nav-item">
+								<a href="#editorialReviews" class="nav-link active" data-bs-toggle="tab"></i>Editorial Reviews</a>
+							</li>
+							<li class="nav-item">
+								<a href="#customerReviews" class="nav-link" data-bs-toggle="tab">Customer Reviews</a>
+							</li>
+						</ul>
+						<div class="tab-content">
+							<div class="tab-pane fade show active" id="editorialReviews">
+								<div class="owl-carousel reviews-carousel owl-theme">
+									<div class="card">
+										<div class="card-header">
+											<p class="review-copy">It's almost like typing on a cloud compared to Apple's Magic Keyboard.</p>
+										</div>
+										<figure class="card-body">
+											<img class="review-icon" src="images/reviews-icon-business_insider.png" />
+											<figcaption>
+												<h5 class="card-title review-name">Antonio Villas-Boas</h5>
+												<p class="review-company">Business Insider</p>
+												<p class="review-focus">On the Das Keyboard 4 Professional for Mac</p>
+											</figcaption>
+										</figure>
+									</div>
+									<div class="card">
+										<div class="card-header">
+											<p class="review-copy">The tactile responsiveness when I push a key is so satisfying. It’s silly but it also makes me feel productive, each keystroke an audio reminder that I’m one step closer to my goal.</p>
+										</div>
+										<figure class="card-body">
+											<img class="review-icon" src="images/reviews-icon-ny_times.png" />
+											<figcaption>
+												<h5 class="card-title review-name">Daisuke Wakabayashi</h5>
+												<p class="review-company">NY Times</p>
+												<p class="review-focus">On the Das Keyboard Model S</p>
+											</figcaption>
+										</figure>
+									</div>
+									<div class="card">
+										<div class="card-header">
+											<p class="review-copy">This highly durable, top-tier mechanical keyboard will satisfy all your click-clacky needs while still offering excellent performance and long-lasting, coat-protected keys.</p>
+										</div>
+										<figure class="card-body">
+											<img class="review-icon" src="images/reviews-icon-digital_trends.png" />
+											<figcaption>
+												<h5 class="card-title review-name">Tyler Lacoma</h5>
+												<p class="review-company">Digital Trends</p>
+												<p class="review-focus">On the Das Keyboard 4 for Mac</p>
+											</figcaption>
+										</figure>
+									</div>
+									<div class="card">
+										<div class="card-header">
+											<p class="review-copy">A formidable and well-designed gaming keyboard with all the usual bells and whistles.</p>
+										</div>
+										<figure class="card-body">
+											<img class="review-icon" src="images/reviews-icon-wired_magazine.png" />
+											<figcaption>
+												<h5 class="card-title review-name">Jess Grey</h5>
+												<p class="review-company">Wired Magazine</p>
+												<p class="review-focus">On the Das Keyboard 5Q</p>
+											</figcaption>
+										</figure>
+									</div>
+									<div class="card">
+										<div class="card-header">
+											<p class="review-copy">Some keyboards just ooze class.</p>
+										</div>
+										<figure class="card-body">
+											<img class="review-icon" src="images/reviews-icon-tech_radar.png" />
+											<figcaption>
+												<h5 class="card-title review-name">Bill Thomas and Kevin Lee</h5>
+												<p class="review-company">Tech Radar</p>
+												<p class="review-focus">On the Das Keyboard Prime 13</p>
+											</figcaption>
+										</figure>
+									</div>
+									<div class="card">
+										<div class="card-header">
+											<p class="review-copy">You’ll probably never enjoy typing more than you will on this keyboard.</p>
+										</div>
+										<figure class="card-body">
+											<img class="review-icon" src="images/reviews-icon-bgr_media.png" />
+											<figcaption>
+												<h5 class="card-title review-name">Tim Kohut</h5>
+												<p class="review-company">BGR Media</p>
+												<p class="review-focus">On the Das Keyboard 4 Professional MX Brown</p>
+											</figcaption>
+										</figure>
+									</div>
+								</div>
+							</div>
+							<div class="tab-pane fade" id="customerReviews">
+								<div class="owl-carousel reviews-carousel owl-theme">
+									<div class="card">
+										<div class="card-header">
+											<p class="review-copy">The tactile responsiveness when I push a key is so satisfying. It’s silly but it also makes me feel productive, each keystroke an audio reminder that I’m one step closer to my goal.</p>
+										</div>
+										<figure class="card-body">
+											<img class="review-icon" src="images/reviews-icon-ny_times.png" />
+											<figcaption>
+												<h5 class="card-title review-name">Daisuke Wakabayashi</h5>
+												<p class="review-company">NY Times</p>
+												<p class="review-focus">On the Das Keyboard Model S</p>
+											</figcaption>
+										</figure>
+									</div>
+									<div class="card">
+										<div class="card-header">
+											<p class="review-copy">The tactile responsiveness when I push a key is so satisfying. It’s silly but it also makes me feel productive, each keystroke an audio reminder that I’m one step closer to my goal.</p>
+										</div>
+										<figure class="card-body">
+											<img class="review-icon" src="images/reviews-icon-ny_times.png" />
+											<figcaption>
+												<h5 class="card-title review-name">Daisuke Wakabayashi</h5>
+												<p class="review-company">NY Times</p>
+												<p class="review-focus">On the Das Keyboard Model S</p>
+											</figcaption>
+										</figure>
+									</div>
+									<div class="card">
+										<div class="card-header">
+											<p class="review-copy">The tactile responsiveness when I push a key is so satisfying. It’s silly but it also makes me feel productive, each keystroke an audio reminder that I’m one step closer to my goal.</p>
+										</div>
+										<figure class="card-body">
+											<img class="review-icon" src="images/reviews-icon-ny_times.png" />
+											<figcaption>
+												<h5 class="card-title review-name">Daisuke Wakabayashi</h5>
+												<p class="review-company">NY Times</p>
+												<p class="review-focus">On the Das Keyboard Model S</p>
+											</figcaption>
+										</figure>
+									</div>
+									<div class="card">
+										<div class="card-header">
+											<p class="review-copy">The tactile responsiveness when I push a key is so satisfying. It’s silly but it also makes me feel productive, each keystroke an audio reminder that I’m one step closer to my goal.</p>
+										</div>
+										<figure class="card-body">
+											<img class="review-icon" src="images/reviews-icon-ny_times.png" />
+											<figcaption>
+												<h5 class="card-title review-name">Daisuke Wakabayashi</h5>
+												<p class="review-company">NY Times</p>
+												<p class="review-focus">On the Das Keyboard Model S</p>
+											</figcaption>
+										</figure>
+									</div>
+									<div class="card">
+										<div class="card-header">
+											<p class="review-copy">The tactile responsiveness when I push a key is so satisfying. It’s silly but it also makes me feel productive, each keystroke an audio reminder that I’m one step closer to my goal.</p>
+										</div>
+										<figure class="card-body">
+											<img class="review-icon" src="images/reviews-icon-ny_times.png" />
+											<figcaption>
+												<h5 class="card-title review-name">Daisuke Wakabayashi</h5>
+												<p class="review-company">NY Times</p>
+												<p class="review-focus">On the Das Keyboard Model S</p>
+											</figcaption>
+										</figure>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	<?php include 'structure/footer.html';?>
+
+	<?php include 'includes/footer-scripts.html';?>
