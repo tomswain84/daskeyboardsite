28c0eccbbc51152e7e1ae75854a81c45b900a3c4
Tom Swain <tom.s@designity.com>
2022-04-18 01:20:30 -0700

    Edit

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 						</figure>
 					</div>
                     <div class="col-12 d-flex py-4">
-                        <a class="btn btn-outline-primary text-gray-dark mx-auto" href="products/mechanical-keyboards?filter=all" title="Shop All Keyboards">Shop All Keyboards<i class="fa-solid fa-plus ms-2"></i></a>
+                        <a class="btn btn-outline-primary text-gray-dark mx-auto" href="products/category-mechanical-keyboards?filter=all" title="Shop All Keyboards">Shop All Keyboards<i class="fa-solid fa-plus ms-2"></i></a>
                     </div>
 				</div>
 				<?php include './parts/part-category-keyboards-subcats.php';?>
