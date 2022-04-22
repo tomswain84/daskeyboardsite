810521d17c40d46562fa209ca88bc6cdf88982e2
Tom Swain <tom.s@designity.com>
2022-04-22 11:21:02 -0700

    Update (Product Page Completed)
    DK X50Q Product page compelted.

--- products/daskeyboard-x50q.php
+++ products/daskeyboard-x50q.php
@@ -10,6 +10,7 @@ $page_title = "Das Keyboard X50Q";
 <?php include '../products/product-details/details-daskeyboard-x50q.php';?>
 <!-- Switch Audio -->
 <?php include '../parts/part-embedded-audio.php';?>
+
         <section class="bg-white section-pad">
         	<div class="container-boxed">
         		<div class="row">
@@ -386,8 +387,119 @@ $page_title = "Das Keyboard X50Q";
 					</div>
 				</div>
 
+				<div class="row align-items-center py-5 my-md-0 border-bottom">
+					<div class="col-md">
+                        <figure class="heading m-0">
+                            <figcaption>
+                                <h2>
+                                    <small>Keypress Precision</small>
+                                    <br>WASD keycaps included
+                                </h2>
+                            </figcaption>
+                            <p> Accurate key presses are mandatory. The included gaming textured WASD keycap upgrade kit will give you a huge tactical advantage. Precision and style on your path to total domination. Key puller included as well. </p>
+                        </figure>
+					</div>
+					<div class="col-md text-center">
+						<img class="img-fluid blend-plus-lighter" src="../images/product-images/product-details/x50q/wasd-shapes-rgb.png" alt="WASD keycaps included" />
+					</div>
+				</div>
+
 			</div>
 		</section><!-- /premiumMaterials -->
+
+		<section id="keyboardSpects" class="bg-gray-darker dark-section section-pad"><!-- /keyboardSpects -->
+			<div id="contentContainer" class="container-boxed">
+				<div class="row">
+					<div class="col">
+						<h2>Specifications</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<h3 class="text-red mb-2">Product Information</h3>
+						<ul class="list-unstyled">
+							<li>Backlighting: RGB</li>
+							<li>Connection Type: USB 2.0</li>
+							<li>Keycap Material: ABS</li>
+							<li>Aluminum top panel: Yes</li>
+							<li>USB Ports (Built-in): No</li>
+							<li>Dedicated Media Controls: Volume Knob, Play/Pause, Track Forward/Backward</li>
+							<li>Easy Access Media Controls: Yes</li>
+							<li>Firmware Updatable: Yes</li>
+						</ul>
+						<h3 class="text-red mb-2">System Requirements</h3>
+						<ul class="list-unstyled">
+							<li>Windows 10</li>
+							<li>One 2.0 USB port</li>
+							<li>Internet connection for software download</li>
+							<li>This keyboard is compatible with Windows only.</li>
+						</ul>
+						<h3 class="text-red mb-2">Warranty Information</h3>
+						<ul class="list-unstyled">
+							<li>1-Year Limited Hardware Warranty</li>
+						</ul>
+						<h3 class="text-red mb-2">Package Content</h3>
+						<ul class="list-unstyled">
+							<li>Keyboard</li>
+							<li>Palm rest</li>
+							<li>WASD two-tone keycaps</li>
+							<li>Keycap puller</li>
+							<li>User documentation</li>
+						</ul>
+						<h3 class="text-red mb-2">User Manual</h3>
+						<a class="btn btn-outline-primary" href="https://docs.google.com/document/d/1I79ZG7MzYzkSWdniAYGYTSsBkf85drA7lVeI6Q03BTU/edit?usp=sharing" target="_blank" title="View the X50Q User Manual">View the X50Q User Manual</a>
+					</div>
+					<div class="col">
+						<h3 class="text-red mb-2">Physical Specifications</h3>
+						<ul class="list-unstyled">
+							<li><strong>Height</strong>: 1.28 in (3.2 cm)</li>
+							<li><strong>Width</strong>: 6.63 in (16.8 cm)</li>
+							<li><strong>Length</strong>: 18 in (45.7 cm)</li>
+							<li><strong>Weight</strong>: 3.5 lbs (1.55 kg)</li>
+							<li><strong>Cable</strong>: 6.5 ft (2 m)</li>
+						</ul>
+						<h3 class="text-red mb-2">Gamma Zulu Switches made by Omron for Das Keyboard</h3>
+						<ul class="list-unstyled">
+							<li><strong>Feel</strong>:Soft tactile</li>
+							<li><strong>Gold Cross Point Contacts</strong>:2</li>
+							<li><strong>Durability</strong>:100 million actuations</li>
+							<li><strong>Actuation distance</strong>:0.06 in (1.5 mm)</li>
+							<li><strong>Actuation force</strong>:1.6 oz (45 g)</li>
+							<li><strong>Total travel distance:</strong>:0.14 in (3.5 mm)</li>
+						</ul>
+						<h3 class="text-red mb-2">Available Layouts</h3>
+						<ul class="list-unstyled">
+							<li>104 US</li>
+							<li>105 UK</li>
+							<li>105 DE</li>
+							<li>105 NO</li>
+						</ul>
+						<h3 class="text-red mb-2">Software</h3>
+						<a class="btn btn-outline-primary" href="https://www.daskeyboard.io/" target="_blank" title="Download Das Keyboard Q Software">Download Das Keyboard Q Software</a>
+						<p>Additional language keycaps are available as an add-on. (See accessories below.)</p>
+					</div>
+				</div>
+				<div class="row mt-5">
+					<div class="col">
+						<h2>Accessories</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<h3 class="text-red mb-2">Translucent Keycap Set</h3>
+						<ul class="list-unstyled">
+							<li>Without inscriptions for 104 and 105 key layouts.</li>
+							<li>With inscriptions for US, UK, German, French, Nordic, Spanish, and Russian.</li>
+							<li>With inscriptions for Dvorak Layout</li>
+						</ul>
+						<h3 class="text-red mb-2">Black, Professional Font Keycap Set</h3>
+						<ul class="list-unstyled">
+							<li>With inscriptions, available only in US</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section><!-- /keyboardSpects -->
 
 <?php require '../structure/footer.php';?>
 <?php require '../includes/footer-scripts.php';?>
