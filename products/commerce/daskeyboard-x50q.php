12f353bdba6605d62d7cd8cd418668c8986c1b04
Tom Swain <tom.s@designity.com>
2022-04-22 21:48:53 -0700

    Edits

--- products/commerce/daskeyboard-x50q.php
+++ products/commerce/daskeyboard-x50q.php
@@ -1,5 +1,5 @@
 <?php $body_id = "x50q";?>
-<?php $body_class = "product-page commerce keyboards q-series";?>
+<?php $body_class = "commerce keyboards q-series";?>
 <?php require '../../includes/head.php';?>
 <?php require '../../structure/header.php';?>
 <!-- Product Dtails -->
@@ -12,15 +12,15 @@ $page_title = "Das Keyboard X50Q";
 	        <section class="bg-white section-pad">
 	        	<div class="container-boxed">
 	        		<div class="row">
-	        			<div id="contentContainer" class="col-md-9">
+	        			<div id="contentContainer" class="col-md-6 col-lg-8 pb-5 pb-md-0">
 	        				<div class="row mb-5">
-	        					<div class="col">
-			        				<a class="btn btn-outline-primary text-gray-dark ms-2" href="<?=$product_url?>" title="Back to Product Details"><i class="fa-solid fa-backward-step me-2"></i>Back to Product Details</a>
+	        					<div class="col text-center text-md-start">
+			        				<a class="btn btn-outline-primary text-gray-dark ms-md-2" href="<?=$product_url?>" title="Back to Product Details"><i class="fa-solid fa-backward-step me-2"></i>Back to Product Details</a>
 			        			</div>
 			        		</div>
 	        				<img class="img-fluid" src="<?=$commerce_image_url?>" alt="<?=$product_title?>" />
 	        			</div>
-	        			<div id="sidebar" class="col-md-3">
+	        			<div id="sidebar" class="col-md-6 col-lg-4">
 	        				<div class="sidebar-content">
 		        				<h2>Das Keyboard <?=$product_title?></h2>
 		        				<span class="star-rating text-red small">
@@ -36,29 +36,29 @@ $page_title = "Das Keyboard X50Q";
 		        					<h3>Starting At: <span class="text-red">$<?=$product_price?></span>
 		        				</div>
 
-		        				<img class="my-4" src="../../images/temp-klarma.png" alt="Temp Klarma" />
+		        				<img class="img-fluid my-4" src="../../images/temp-klarma.png" alt="Temp Klarma" />
 
 		        				<div class="option-switches mb-5">
 			        				<h3 class="sidebar-title">Select Switch Type</h3>
-			        				<div class="form-check my-3">
+			        				<div class="form-check my-3 ps-0">
 			        					<input class="form-check-input visually-hidden" type="radio" name="flexRadioDefault" id="swtichClicky" checked="checked">
 			        					<label class="switch-select form-check-label d-flex align-items-center" for="swtichClicky">
 			        						<img class="select-image me-3" src="../../images/commerce_options-switch_type-clicky.png" alt="Clicky" />
-			        						<span class="label-wrap"><span class="oswald">Clicky</span> <small>- Cherry MX Blue</small></span>
+			        						<span class="label-wrap"><span class="oswald">Clicky</span><small>&nbsp-&nbspCherry MX Blue</small></span>
 			        					</label>
 			        				</div>
-			        				<div class="form-check my-3">
+			        				<div class="form-check my-3 ps-0">
 			        					<input class="form-check-input visually-hidden" type="radio" name="flexRadioDefault" id="switchSoftTactile">
 			        					<label class="switch-select form-check-label d-flex align-items-center" for="switchSoftTactile">
 			        						<img class="select-image me-3" src="../../images/commerce_options-switch_type-soft_tactile.png" alt="Soft Tactile" />
-			        						<span class="label-wrap"><span class="oswald">Soft Tactile</span> <small>- Cherry MX Brown</small></span>
+			        						<span class="label-wrap"><span class="oswald">Soft Tactile</span><small>&nbsp-&nbspCherry MX Brown</small></span>
 			        					</label>
 			        				</div>
-			        				<div class="form-check my-3">
+			        				<div class="form-check my-3 ps-0">
 			        					<input class="form-check-input visually-hidden" type="radio" name="flexRadioDefault" id="selectSmoothLinear">
 			        					<label class="switch-select form-check-label d-flex align-items-center" for="selectSmoothLinear">
 			        						<img class="select-image me-3" src="../../images/commerce_options-switch_type-smooth_linear.png" alt="Smooth Linear" />
-			        						<span class="label-wrap"><span class="oswald">Smooth Linear</span> <small>- Cherry MX Red</small></span>
+			        						<span class="label-wrap"><span class="oswald">Smooth Linear</span><small>&nbsp-&nbspCherry MX Red</small></span>
 			        					</label>
 			        				</div>
 			        			</div>
