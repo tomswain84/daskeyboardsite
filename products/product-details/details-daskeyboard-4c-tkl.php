a78b77bbda80e3d2bb0e0d6a582f1592b03fd841
Tom Swain <101903058+tomswain84@users.noreply.github.com>
2022-05-04 16:57:19 -0700

    Update details-daskeyboard-4c-tkl.php

--- products/product-details/details-daskeyboard-4c-tkl.php
+++ products/product-details/details-daskeyboard-4c-tkl.php
@@ -2,7 +2,7 @@
 $product_title = "4C TKL";
 $product_image_url = "/sites/daskeyboard/images/product-images/cards/img-daskeyboard-4c-tkl.png";
 $buynow_url = "/sites/daskeyboard/products/commerce/daskeyboard-4c-tkl";
-$product_url = "/sites/daskeyboard/products/daskeyboard-4c-tkl";
+$product_url = "/sites/daskeyboard/products/daskeyboard-4C-tkl";
 $product_price = "139";
 $product_tags = "for-pc supercharged aluminum linux";
 $keystroke = "50";
