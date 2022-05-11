5e3475bd984700893295dbd53e6a6e6ca6f73481
Tom Swain <101903058+tomswain84@users.noreply.github.com>
2022-05-11 08:23:33 -0700

    Update
    Added: Account Creation
    Added: Account Recovery
    
    All within the same modal system. Neither clicking outside of the modal nor the esc key will close them.

--- structure/header.php
+++ structure/header.php
@@ -5,11 +5,129 @@
                     <div class="col">
                         <nav class="navbar p-0 navbar-expand-sm navbar-dark">
 
-                            <?php include './parts/part-secondary-nav-modal-shipping.php';?>
+                            <!-- Modal Trigger - Shipping Info -->
+                            <a class="shipping-notice order-2 py-3" href="#" title="Free Shipping" data-bs-toggle="modal" data-bs-target="#freeShipping">
+                                Free Shipping <span class="d-none d-md-inline">(Click for Details)</span>
+                            </a>
+                            <!-- Modal - Shipping Info -->
+                            <div class="modal fade" id="freeShipping" tabindex="-1" aria-labelledby="freeShipping" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content bg-gray-darker">
+                                        <div class="modal-header bg-gray-dark text-white border-bottom">
+                                            <h5 class="modal-title" id="freeShipping">Shipping Info</h5>
+                                            <a href="#" class="btn btn-outline-primary" data-bs-dismiss="modal">Close<i class="fa-solid fa-xmark ms-2"></i></a>
+                                        </div>
+                                        <div class="modal-body bg-gray-darker">
+                                            <h2 class="text-white"><span class="text-red">Free Shipping</span> Everyday on Orders Over $59</h2>
+                                            <p class="text-white">All orders over $59 qualify for our Free Shipping offer within the continental United States. Keyboards are shipped via UPS Ground, and accessories and swag may be shipped via USPS. Once your cart meets this minimum amount, the Free Shipping option is automatically available during checkout. Other expedited shipping options will also be available at an additional charge.</p>
+                                        </div>
+                                        <div class="modal-footer bg-gray-dark border-top">
+                                            <a href="/sites/daskeyboard/about-us" class="btn btn-primary">Learn More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-                            <?php include './parts/part-secondary-nav-modal-account-login.php';?>
+                            <!-- Modal Trigger - Account Login -->
+                            <a class="account-login d-flex align-items-center text-decoration-none order-3 py-3 px-sm-2" href="#" title="Account Login" data-bs-toggle="modal" data-bs-target="#accountLogin">
+                                <i class="fa-solid fa-user text-red"></i>
+                                <span class="ms-2 small">My Account</span>
+                                <i class="d-none fa-solid fa-caret-down text-white small ms-1"></i>
+                            </a>
+                            <!-- Modal - Account Login -->
+                            <form class="modal fade" id="accountLogin" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="accountLogin" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content bg-gray-darker">
+                                        <div class="modal-header bg-gray-dark text-white border-bottom">
+                                            <h5 class="modal-title" id="accountLogin">Registered Customers</h5>
+                                            <a href="#" class="btn btn-outline-primary" data-bs-dismiss="modal">Close<i class="fa-solid fa-xmark ms-2"></i></a>
+                                        </div>
+                                        <div class="modal-body bg-gray-darker">
+                                            <h2 class="text-white"><span class="text-red">Login</span> to Your Account</h2>
+                                            <p class="text-white mb-5">If you have an account with us, please log in.</p>
 
-                            <div class="dropdown d-block d-sm-none py-3">
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text border-0 rounded-0 bg-gray-dark text-white" id="email"><i class="fa-solid fa-envelope"></i></span>
+                                                <input type="text" class="form-control rounded-0 border-0 bg-gray-light oswald text-white" placeholder="Email" aria-label="email" aria-describedby="email">
+                                            </div>
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text border-0 rounded-0 bg-gray-dark text-white" id="password"><i class="fa-solid fa-unlock-keyhole"></i></span>
+                                                <input type="password" id="inputPassword" class="form-control rounded-0 border-0 bg-gray-light oswald text-white" placeholder="Password" aria-label="password" aria-describedby="password">
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer bg-gray-dark border-top">
+                                            <a class="text-white text-decoration-none me-auto" href="#" title="Forgot Password" data-bs-toggle="modal" data-bs-target="#forgotPassword">Forgot Password?</a>
+                                            <a class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#createAccount">Create an Account</a>
+                                            <a type="submit" class="btn btn-primary">Login<i class="fa-solid fa-right-to-bracket ms-2"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+
+                            <!-- Modal - Forgot Password -->
+                            <form class="modal fade" id="forgotPassword" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="forgotPassword" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content bg-gray-darker">
+                                        <div class="modal-header bg-gray-dark text-white border-bottom">
+                                            <h5 class="modal-title" id="forgotPassword">Account Recovery</h5>
+                                            <a href="#" class="btn btn-outline-primary" data-bs-dismiss="modal">Close<i class="fa-solid fa-xmark ms-2"></i></a>
+                                        </div>
+                                        <div class="modal-body bg-gray-darker">
+                                            <h2 class="text-white"><span class="text-red">Reset</span> your password</h2>
+                                            <p class="text-white mb-5">We will send you an email to reset your password.</p>
+
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text border-0 rounded-0 bg-gray-dark text-white" id="email"><i class="fa-solid fa-envelope"></i></span>
+                                                <input type="text" class="form-control rounded-0 border-0 bg-gray-light oswald text-white" placeholder="Email" aria-label="email" aria-describedby="email">
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer bg-gray-dark border-top">
+                                            <a class="text-white text-decoration-none me-auto" href="#" title="Forgot Password" data-bs-toggle="modal" data-bs-target="#accountLogin"><i class="fa-solid fa-caret-left me-2"></i>Back to Login</a>
+                                            <a class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#createAccount">Create an Account</a>
+                                            <a type="submit" class="btn btn-primary">Reset<i class="fa-solid fa-unlock-keyhole ms-2"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+
+                            <!-- Modal - Create An Account -->
+                            <form class="modal fade" id="createAccount" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="createAccount" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content bg-gray-darker">
+                                        <div class="modal-header bg-gray-dark text-white border-bottom">
+                                            <h5 class="modal-title" id="createAccount">New Customers</h5>
+                                            <a href="#" class="btn btn-outline-primary" data-bs-dismiss="modal">Close<i class="fa-solid fa-xmark ms-2"></i></a>
+                                        </div>
+                                        <div class="modal-body bg-gray-darker">
+                                            <h2 class="text-white mb-5"><span class="text-red">Create</span> your Account</h2>
+
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text border-0 rounded-0 bg-gray-dark text-white" id="first-name"><i class="fa-solid fa-user"></i></span>
+                                                <input type="text" class="form-control rounded-0 border-0 bg-gray-light oswald text-white" placeholder="First Name" aria-label="first-name" aria-describedby="first-name">
+                                            </div>
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text border-0 rounded-0 bg-gray-dark text-white" id="last-name"><i class="fa-solid fa-user"></i></span>
+                                                <input type="text" class="form-control rounded-0 border-0 bg-gray-light oswald text-white" placeholder="Last Name" aria-label="last-name" aria-describedby="last-name">
+                                            </div>
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text border-0 rounded-0 bg-gray-dark text-white" id="email"><i class="fa-solid fa-envelope"></i></span>
+                                                <input type="text" class="form-control rounded-0 border-0 bg-gray-light oswald text-white" placeholder="Email" aria-label="email" aria-describedby="email">
+                                            </div>
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text border-0 rounded-0 bg-gray-dark text-white" id="password"><i class="fa-solid fa-unlock-keyhole"></i></span>
+                                                <input type="password" id="inputPassword" class="form-control rounded-0 border-0 bg-gray-light oswald text-white" placeholder="Password" aria-label="password" aria-describedby="password">
+                                            </div>
+
+                                        </div>
+                                        <div class="modal-footer bg-gray-dark border-top">
+                                            <a class="text-white text-decoration-none me-auto" href="#" title="Forgot Password" data-bs-toggle="modal" data-bs-target="#accountLogin"><i class="fa-solid fa-caret-left me-2"></i>Back to Login</a>
+                                            <a type="submit" class="btn btn-primary">Create Account<i class="fa-solid fa-plus ms-2"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+
+                            <div class="dropdown d-block d-sm-none py-2">
                                 <a class="navbar-toggler dropdown-toggle" type="button" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
                                     <span class="toggler-icon">
                                         <span class="w-75 ms-auto"></span>
@@ -18,13 +136,35 @@
                                     </span>
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-start bg-gray-dark border-0 rounded-0" aria-labelledby="navbarDropdown">
-                                    <?php include './parts/part-secondary-nav-dropdown-links.php';?>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="https://daskeyboard.mojohelpdesk.com/" target="_blank">Support</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/sites/daskeyboard/blog">Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/sites/daskeyboard/about-us">About Us</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Wishlist</a>
+                                    </li>
                                 </ul>
                             </div>
 
                             <div class="collapse navbar-collapse d-none order-sm-2" id="navbarNavDropdown">
                                 <ul class="navbar-nav ms-auto me-3 mt-3 mt-sm-0">
-                                    <?php include './parts/part-secondary-nav-dropdown-links.php';?>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="https://daskeyboard.mojohelpdesk.com/" target="_blank">Support</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/sites/daskeyboard/blog">Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/sites/daskeyboard/about-us">About Us</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Wishlist</a>
+                                    </li>
                                 </ul>
                             </div>
                         </nav>
